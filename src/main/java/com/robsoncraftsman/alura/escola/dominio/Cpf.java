6d924a642bba7dc85dc0d073c54ae239cbd495da
Robson S. Rodrigues <robsoncraftsman@gmail.com>
2020-10-19 16:23:20 -0300

    Implementação do repositório de aluno

--- src/main/java/com/robsoncraftsman/alura/escola/dominio/Cpf.java
+++ src/main/java/com/robsoncraftsman/alura/escola/dominio/Cpf.java
@@ -1,5 +1,6 @@
 package com.robsoncraftsman.alura.escola.dominio;
 
+import java.util.Objects;
 
 public class Cpf {
 
@@ -14,6 +15,32 @@ public class Cpf {
 
 	public String getValor() {
 		return this.valor;
+	}
+
+	@Override
+	public int hashCode() {
+		final var prime = 31;
+		var result = 1;
+		result = (prime * result) + ((this.valor == null) ? 0 : this.valor.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(final Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		final var other = (Cpf) obj;
+		if (!Objects.equals(this.valor, other.valor)) {
+			return false;
+		}
+		return true;
 	}
 
 }
